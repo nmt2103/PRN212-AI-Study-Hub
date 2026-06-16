e05079a1920b4c791a4fa4cbdabc247f450c175e
Ngo Minh Thuan <nmt0321@gmail.com>
2026-06-16 18:29:52 +0700

    Add project files.

---
+++ Prn212.AIStudyHub.WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Prn212.AIStudyHub.WPF
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+  }
+
+}
