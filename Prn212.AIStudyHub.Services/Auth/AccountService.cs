745739247517bb0a99492d0d74fe582de369b11a
MSI\saita <saitama0102huy@gmail.com>
2026-07-06 16:10:19 +0700

    feat: implement profile update UI - Add update profile feature - Add popup menu to MainWindow - Add custom button styles

--- Prn212.AIStudyHub.Services/Auth/AccountService.cs
+++ Prn212.AIStudyHub.Services/Auth/AccountService.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Prn212.AIStudyHub.DataAccess;
 
 namespace Prn212.AIStudyHub.Services.Auth;
@@ -7,21 +8,33 @@ namespace Prn212.AIStudyHub.Services.Auth;
 /// </summary>
 public class AccountService
 {
-  public void UpdateProfile(int userId, string firstName, string lastName)
-  {
-    // TODO (AUTH 2): lấy user theo id, cập nhật tên, SaveChanges.
-    throw new NotImplementedException("Nhóm AUTH cài đặt UpdateProfile tại đây.");
-  }
+    private readonly AistudyHubDbContext _context = new();
 
-  public void RequestPasswordReset(string email)
-  {
-    // TODO (AUTH 2): sinh token, gửi email.
-    throw new NotImplementedException("Nhóm AUTH cài đặt RequestPasswordReset tại đây.");
-  }
+    public async Task UpdateProfile(string email, string firstName, string lastName)
+    {
+        AppUser? user = await _context.AppUsers.FirstOrDefaultAsync(e => e.Email.Equals(email));
+        if (user == null) throw new Exception("User doesn't exist in the system.");
+        user.FirstName = firstName;
+        user.LastName = lastName;
+        await _context.SaveChangesAsync();
+    }
 
-  public void ResetPassword(string email, string token, string newPassword)
-  {
-    // TODO (AUTH 2): kiểm tra token, hash mật khẩu mới, cập nhật.
-    throw new NotImplementedException("Nhóm AUTH cài đặt ResetPassword tại đây.");
-  }
+    public async Task<AppUser> GetCurrentUser(string email)
+    {
+        var user = await _context.AppUsers.FirstOrDefaultAsync(e => e.Email.Equals(email));
+        if (user == null) throw new Exception("User doesn't exist in the system.");
+        return user;
+    }
+
+    public void RequestPasswordReset(string email)
+    {
+        // TODO (AUTH 2): sinh token, gửi email.
+        throw new NotImplementedException("Nhóm AUTH cài đặt RequestPasswordReset tại đây.");
+    }
+
+    public void ResetPassword(string email, string token, string newPassword)
+    {
+        // TODO (AUTH 2): kiểm tra token, hash mật khẩu mới, cập nhật.
+        throw new NotImplementedException("Nhóm AUTH cài đặt ResetPassword tại đây.");
+    }
 }
